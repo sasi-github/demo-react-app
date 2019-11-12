a595be4beb7d13ceaa83718edc46b6909198ca0f
sasi-git <sasimails2002@yahoo.com>
2019-11-12 22:43:07 +0000

    Integrated API in React Page

--- src/App.js
+++ src/App.js
@@ -41,18 +41,10 @@ render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            My First React App
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
         <p>{this.state.response}</p>
         <form onSubmit={this.handleSubmit}>
